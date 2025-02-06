bf8b1dcb8768e9f37aa05b47a1da7d26955fe870
mihaly-ale <105678319+mihaly-ale@users.noreply.github.com>
2025-02-06 11:57:25 +0100

    Update button text and message based on current state (Log in / Create Account)

--- script.js
+++ script.js
@@ -2,8 +2,18 @@ const toggleBtn = document.getElementById('toggle-button');
 const regForm = document.getElementById('reg-form')
 const signinForm = document.getElementById('signin-form')
 
+function changeButtonText () {
+  const currentButtonText = this.textContent;
+  let message = this.previousElementSibling;
+
+  currentButtonText === "Log in" ?
+    (this.textContent = "Create Account", message.textContent = "Don'have an account yet?") :
+    (this.textContent = "Log in", message.textContent = "Already have an account?");
+}
+
 toggleBtn.addEventListener('click', function () {
   regForm.toggleAttribute("data-invisible")
   signinForm.toggleAttribute("data-invisible")
+  changeButtonText.call(this)
 })
 
