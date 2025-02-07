885352cf3b21430601f3c0a4d9270fca0bb48a78
mihaly-ale <105678319+mihaly-ale@users.noreply.github.com>
2025-02-07 13:22:22 +0100

    Extract form toggling to functions
    - Used setAttribute to add explicit values to the data-invisible
      attribute, this way the click event listener can invoke functions
      depending on the value: true or false.
      This is only for clarity, as  toggleAttribute added empty values.

--- script.js
+++ script.js
@@ -1,19 +1,33 @@
 const toggleBtn = document.getElementById('toggle-button');
-const regForm = document.getElementById('reg-form')
-const signinForm = document.getElementById('signin-form')
+const regForm = document.getElementById('reg-form');
+const signinForm = document.getElementById('signin-form');
 
-function changeButtonText () {
-  const currentButtonText = this.textContent;
-  let message = this.previousElementSibling;
+function changeButtonText() {
+	const currentButtonText = this.textContent;
+	let message = this.previousElementSibling;
 
-  currentButtonText === "Log in" ?
-    (this.textContent = "Create Account", message.textContent = "Don't have an account yet?") :
-    (this.textContent = "Log in", message.textContent = "Already have an account?");
+	currentButtonText === 'Log in'
+		? ((this.textContent = 'Create Account'),
+		  (message.textContent = "Don't have an account yet?"))
+		: ((this.textContent = 'Log in'),
+		  (message.textContent = 'Already have an account?'));
+}
+
+function displaySignInform() {
+	signinForm.setAttribute('data-invisible', 'false');
+	regForm.setAttribute('data-invisible', 'true');
+}
+
+function displayRegform() {
+	signinForm.setAttribute('data-invisible', true);
+	regForm.setAttribute('data-invisible', false);
 }
 
 toggleBtn.addEventListener('click', function () {
-  regForm.toggleAttribute("data-invisible")
-  signinForm.toggleAttribute("data-invisible")
-  changeButtonText.call(this)
-})
+	signinForm.dataset.invisible === 'true'
+		? displaySignInform()
+		: displayRegform();
+
+	changeButtonText.call(this);
+});
 
