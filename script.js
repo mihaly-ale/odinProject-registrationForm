77b31f3ca4631dbc033aa70ae4beebe2c9add95d
Mihaly Ale <105678319+mihaly-ale@users.noreply.github.com>
2025-02-13 10:42:27 +0100

    Fix form submission bug
    - Form is only submitted if it satisfies the passwordMatchCheck function
      outcome.

--- script.js
+++ script.js
@@ -47,8 +47,8 @@ regFormSubmitBtn.addEventListener('click', (e) => {
 	} else {
 		if (passwordMatchCheck() === true) {
 			console.log(`Successful submission.`);
+			regForm.submit();
 		}
-		regForm.submit();
 	}
 });
 
